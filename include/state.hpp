53f84f7a5343ee359d4a8fa65c30a9a8386cf3b6
cypox <am_neggaz@esi.dz>
2022-10-20 11:44:56 +0200

    added optimization logic

--- include/state.hpp
+++ include/state.hpp
@@ -24,16 +24,41 @@ public:
   void update_rate(const std::string& key1, const std::string& key2, const double bid, const double ask) {
     int idx1 = currencies_indices[key1];
     int idx2 = currencies_indices[key2];
-    rates[idx1][idx2] = {bid, ask};
+    rates[idx2][idx1] = {bid, ask}; // invert to have rows corresponding to "from" and columns to "to"
+  }
+
+  void optimize() {
+    // apsp on the rates matrix to update the costs
+    int n = currencies_indices.size();
+    for (int k = 0 ; k < n ; ++ k)
+    {
+      for (int i = 0 ; i < n ; ++ i)
+      {
+        for (int j = 0 ; j < n ; ++ j)
+        {
+          //if (i == j || i == k || k == j) continue;
+          double new_bid_price = rates[i][k].first * rates[k][j].first;
+          rates[i][j].first = std::min(rates[i][j].first, new_bid_price);
+          double new_ask_price = rates[i][k].second * rates[k][j].second;
+          rates[i][j].second = std::max(rates[i][j].second, new_ask_price);
+        }
+      }
+    }
   }
 
   void show_rates() const {
     int n = currencies_indices.size();
+    printf("     ");
+    for (auto& k: currencies_indices)
+      printf("     %s     ", k.first.c_str());
+    printf("\n");
+    auto it = currencies_indices.begin();
     for (int i = 0 ; i < n ; ++ i)
     {
+      printf(" %s ", (it++)->first.c_str());
       for (int j = 0 ; j < n ; ++ j)
       {
-        printf("%f %f", rates[i][j].first, rates[i][j].second);
+        printf(" [%.2f %.2f] ", rates[i][j].first, rates[i][j].second);
       }
       printf("\n");
     }
@@ -42,6 +67,7 @@ public:
 private:
   std::unordered_map<std::string, int> currencies_indices;
   std::vector<std::vector<std::pair<double, double>>> rates;
+  std::vector<std::vector<std::pair<double, double>>> shortest_paths;
 };
 
 }
--- src/fxwiz.cpp
+++ src/fxwiz.cpp
@@ -16,11 +16,22 @@ int main(int argc, char** argv)
   fxwiz::retriever r;
   std::string res = r.fetch(argv[1]);
 
-  fxwiz::parser p;
-  p.parse_content(res);
+  //fxwiz::parser p;
+  //p.parse_content(res);
 
-  const fxwiz::state& s = p.get_state();
-  s.show_rates();
+  //const fxwiz::state& s = p.get_state();
+  //s.show_rates();
+
+  fxwiz::state test_state({"EUR", "USD", "GBP"});
+  test_state.update_rate("EUR", "USD", 0.97825, 0.97842);
+  test_state.update_rate("EUR", "GBP", 0.87155, 0.87173);
+  test_state.update_rate("USD", "EUR", 1.12200, 1.12245);
+  test_state.update_rate("USD", "GBP", 0.89094, 0.89114);
+  test_state.update_rate("GBP", "EUR", 1.14700, 1.14725);
+  test_state.update_rate("GBP", "USD", 1.12216, 1.12241);
+  test_state.show_rates();
+  test_state.optimize();
+  test_state.show_rates();
 
   return 0;
 }
